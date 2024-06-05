0232579b7cfc5e567645d9d5b7f1aec975bf1e0a
leminhhuybg <1>
2024-06-06 05:57:20 +0700

    Authtication Part2

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,7 +8,35 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "41f5ccd8-13d2-4c23-94fd-621d3ac4f99c";
+            var writeRoleId = "a311b820-c6bb-4711-a5dc-b695e588eaba";
+
+            var roles = new List<IdentityRole> 
+            { 
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                },
+                new IdentityRole
+                {
+                    Id = writeRoleId,
+                    ConcurrencyStamp = writeRoleId,
+                    Name = "Write",
+                    NormalizedName = "Write".ToUpper(),
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
